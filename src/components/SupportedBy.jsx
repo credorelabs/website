dbd577caf59af0d3b251d89ef1e9b9a66873d664
rajkishorepatra <rpatrasm@gmail.com>
2024-09-24 17:03:00 +0530

    add:Mr Shastry photo; remove: finnovate

--- src/components/SupportedBy.jsx
+++ src/components/SupportedBy.jsx
@@ -7,6 +7,7 @@ import Vinners from "../../public/assets/images/partners/vinners.png";
 import SPJMR from "../../public/assets/images/partners/spjmr.png";
 import STPI from "../../public/assets/images/partners/STPI-Gurgaon.png";
 import KIIT from "../../public/assets/images/partners/KIIT-TBI.png";
+import Meity from "../../public/assets/images/partners/Meity.png";
 
 const SupportedBy = () => {
   return (
@@ -16,6 +17,12 @@ const SupportedBy = () => {
           <div className="p-5 pl-0 mb-10 flex flex-col gap-1">
             <h1 className="text-base text-left p-3 py-2 bg-[#29564b85] text-white">Investors</h1>
             <div className="flex items-center border-1">
+            <div className="px-2 border-r-2 border-grey-100 cursor-pointer">
+                <Link href="https://www.padup.in/fop">
+                  {/* <Image src={PadUp} width={310} height={140} alt="Padup Ventures" /> */}
+                  <Image src={Meity} alt="MEITY" width={300} height={100}/>
+                </Link>
+              </div>
               <div className="px-2 border-r-2 border-grey-100 cursor-pointer">
                 <Link href="https://www.padup.in/fop">
                   {/* <Image src={PadUp} width={310} height={140} alt="Padup Ventures" /> */}
@@ -33,11 +40,11 @@ const SupportedBy = () => {
           <div className="p-5 pl-0 mb-10 flex flex-col gap-1">
             <h1 className="text-base text-left p-3 py-2 bg-[#29564b85] text-white">Supported By</h1>
             <div className="flex items-center border-1">
-              <div className="px-2 border-r-2 border-grey-100 cursor-pointer">
+              {/* <div className="px-2 border-r-2 border-grey-100 cursor-pointer">
                 <Link href="https://www.spjimr.org/finnovate-accelerator/about">
                   <Image src={SPJMR} width={"410"} height={"140"} alt="SPJMR" />
                 </Link>
-              </div>
+              </div> */}
               <div className="px-2 border-x-2 border-grey-100 cursor-pointer">
                 <Link href="https://apiary.stpi.in/">
                   <Image src={STPI} width={"308"} height={"140"} alt="STPI Gurgaon" />
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -10,6 +10,7 @@ import Abhilash from "../../public/assets/images/team/Abhilash.jpeg";
 import Praduman from "../../public/assets/images/team/Praduman.jpeg";
 import Raj from "../../public/assets/images/team/Raj.jpeg";
 import Sunil from "../../public/assets/images/team/sunil.jpeg";
+import Shastri from "../../public/assets/images/team/Shastri.jpeg";
 // import Step1 from "../../public/assets/images/about/step1.png";
 
 const Team = () => {
@@ -26,7 +27,7 @@ const Team = () => {
   //   </section>
   // );
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900 max-w-screen-lrg mx-auto items-center text-center">
       <div className="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-3 ">
         <div className="mx-auto max-w-screen-sm text-center mb-3 lg:mb-5">
           <h2 className="mb-4 text-4xl tracking-tight font-bold dark:text-white">
@@ -51,7 +52,7 @@ const Team = () => {
               <span className="text-gray-500 dark:text-gray-400 text-xsm font-medium">
                 Founder
               </span>
-              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-sm font-normal">
+              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-xs">
                 15 years of IT experience in SoftwareDevelopment at Barclays,
                 BNY Mellon,HSBC and web3 startups Streams, ZEFI and Herdius.
               </p>
@@ -74,7 +75,7 @@ const Team = () => {
               <span className="text-gray-500 dark:text-gray-400 text-xsm font-medium">
               Chief Business Officer, MBA in Marketing from ITM B-School, Navi Mumbai
               </span>
-              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-sm font-normal">
+              <p className="mt-3 font-light text-gray-500 dark:text-gray-400 text-xs">
               Dinesh has over 12 years of experience in roles related to Sales, Business Development, Strategy & Business Excellence with companies such as Blue Dart, Blackbuck & Paytm.
               </p>
             </div>
@@ -149,7 +150,7 @@ const Team = () => {
           </h2>
         </div>
         <div className="grid mb-6 lg:mb-16 gap-5 med:grid-cols-2">
-        <div className="items-center bg-gray-50 rounded-md shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+          <div className="items-center bg-gray-50 rounded-md shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
             <a href="#" className="px-3">
               <Image
                 className="w-full rounded-md sm:rounded-none sm:rounded-l-lg"
@@ -163,11 +164,11 @@ const Team = () => {
               <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                 <a href="#">Harshit H Jain</a>
               </h3>
-              <span className="text-gray-500 dark:text-gray-400 text-xsm font-medium">
+              <span className="text-gray-500 dark:text-gray-400 text-xs font-medium">
                 Director and Chief Mentor, M.Sc.(Tech) Computer Science fromBITS
                 Pilani and MBA from IIMAhmedabad
               </span>
-              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-sm font-normal">
+              <p className="mt-3 font-light text-gray-500 dark:text-gray-400 text-xs">
                 Harshit has over 30 Years of experience insetting up GTS
                 businesses forBanks/NBFCs/IT companies.
               </p>
@@ -190,12 +191,34 @@ const Team = () => {
               <span className="text-gray-500 dark:text-gray-400 text-xsm font-medium">
               Advisor, MBA in Marketing from Northumbria University, UK
               </span>
-              <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400 text-sm font-normal">
+              <p className="mt-3 font-light text-gray-500 dark:text-gray-400 text-xs">
               Sunil, CEO of XTN Singapore, has worked for more than 2 decades with wide-ranging management and execution experience of IT/ERP/Supply Chain/Trade Finance solutions gained across sectors internationally.
               </p>
             </div>
           </div>
         </div>
+          <div className="items-center bg-gray-50 rounded-md shadow sm:flex dark:bg-gray-800 dark:border-gray-700 med:w-1/2 mx-auto">
+            <a href="#" className="px-3">
+              <Image
+                className="w-full rounded-md sm:rounded-none sm:rounded-l-lg px-3"
+                src={Shastri}
+                width={250}
+                height={250}
+                alt="Rajeswara Sastry Kuchi"
+              />
+            </a>
+            <div className="p-5">
+              <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <a href="#"> Rajeswara Sastry Kuchi</a>
+              </h3>
+              <span className="text-gray-500 dark:text-gray-400 text-xsm font-medium">
+              Strategic Advisor – Global Trade & Business Development
+              </span>
+              <p className="mt-3 font-light text-gray-500 dark:text-gray-400 text-xs">
+              Experienced Head Of Business Development with a demonstrated history of strong business development professional skilled in Vendor Management, Management, Customer Service, Strategic Planning, and Business Development.
+              </p>
+            </div>
+          </div>
       </div>
     </section>
   );
