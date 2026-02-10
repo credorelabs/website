2f16f45811fa3ec334b5456f1d53f121d005e8d3
glorina0 <glorinasenapati105@gmail.com>
2026-02-10 17:42:45 +0530

    remove the console

--- src/components/RequestModal.jsx
+++ src/components/RequestModal.jsx
@@ -16,7 +16,6 @@ function Home({ OnClose }) {
   const [remarks, setRemarks] = useState("");
 
   const url = process.env.NEXT_PUBLIC_URL;
-  console.log(url)
   const OTP_URL = `${url}/auth/email-verify`;
 
 
